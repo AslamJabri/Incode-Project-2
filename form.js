3ae188829b257df0553f4a477a4fef2b40afef38
Aslam Ali <aslamali@Aslam-Jabri.local>
2021-08-18 00:49:30 +1000

    Coding is requried

--- form.js
+++ form.js
@@ -1,1 +1,12 @@
+const submitbutton = document.querySelectorAll('[data-submit-target]')
+const closebutton = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
+
+submitbutton.forEach(button => {
+    button.addEventListener('click',() =>
+    {
+      const popUp =document.querySelectorAll(button.CDATA_SECTION_NODE.submitTarget)
+        
+    })
+})
