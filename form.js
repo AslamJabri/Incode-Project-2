eefd59fc2a0a0e2679cab37e892aaf5cd4ee2e5e
AslamJabri <62886145+AslamJabri@users.noreply.github.com>
2021-08-22 18:34:51 +1000

    Add files via upload
    filnal

--- form.js
+++ form.js
@@ -1,4 +1,49 @@
-const { format } = require("path/posix")
 
-const name = document.querySelector('#email')
-format(setattribute novalide);
+const mail = document.querySelector("#email")
+const form = document.querySelector("#form")
+const modal = document.querySelector("#modal")
+const overlay = document.querySelector("#overlay")
+//regex
+const regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])+$/
+
+
+
+form.setAttribute("novalidate",true)
+form.onsubmit = submitForm
+
+
+function submitForm (event)  {
+    event.preventDefault()
+    if(valid(regex , mail)){
+        console.log(`email : ${mail.value}`)
+    }else{
+        console.log ("please type correct email")
+    }
+}
+
+const valid = (regex ,input) => {
+    return regex.test(mail.value)
+}
+const next = document.getElementById('form')
+    next.onsubmit = next.reset()
+
+
+const openModalButton = document.querySelector('.button')
+const closeModalButton = document.querySelector('.modal-cls')
+
+
+ 
+
+    openModalButton.addEventListener(`click`, () => {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+    })
+
+
+    closeModalButton .addEventListener(`click`, () => {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+    })
+    
